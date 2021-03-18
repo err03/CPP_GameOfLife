987fd94950e2b19fb560d095efea10b1a18bd323
bingr <48603790+err03@users.noreply.github.com>
2021-03-19 04:21:41 +0800

    update and add new files
    Update the w6_project_gameOfLife.cpp and add new DetermineFunction.h ; DetermineFunction.cpp

--- w6_project_gameOfLife.cpp
+++ w6_project_gameOfLife.cpp
@@ -1,41 +1,75 @@
 //============================================================================
 // Name        : w6_project_gameOfLife.cpp
-// Author      : 
+// Author      : Yang,WeiBin ; Zhou,XuanMing
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
 #include <iostream>
+#include <iomanip>
+#include <time.h>
+#include <stdlib.h>
+
+//#include "Graph.h"
+#include "DetermineFunctions.h"
+//#include "Cell.h"
 using namespace std;
 
-void determineValidPosition(int,int);
+void generateCell();
+void drawGraph();
 
 const int ROL = 8,COL = 8;
-int main() {
+char lives[ROL][COL];
+char cell = '*';	//change cell's appearance
 
-	int lives[ROL][COL];
+int main() {
+	generateCell();
+	drawGraph();
+	return 0;
+}//main
+//------------------------------ Graph Functions -------------------------------------------------------
+void drawGraph(){
+	for(int i=0;i<ROL;i++){		//print out the graph
+		for(int c=0;c<COL;c++){
+			if(c == (COL-1)){
+				cout << " " << lives[i][c] << " "<<endl;
+				continue;
+			}//if
+			cout << " "<<lives[i][c]<<" |";
+		}//for
+		if(i != (ROL-1)){
+			cout <<setw(4*COL)<<setfill('-')<< "" <<endl;	//set the separate line by COL length
+		}//if
+	}//for
+}//draw graph
+
+//------------------------------ Cell Functions -------------------------------------------------------
+void generateCell(){
 	for(int i=0;i<ROL;i++){
 		for(int c=0;c<COL;c++){
-			cout << "["<<i<<"]["<<c<<"]  ";
+			lives[i][c] = ' ';
+		}//for
+	}//for
+
+	int numOfCell = 25;		//how many cell you want?
+	srand(time(NULL));
+	for(int i=0;i<numOfCell;i++){
+		int rol = rand() % ROL;
+		int col = rand() % COL;
+		if(lives[rol][col] != cell){
+			lives[rol][col] = cell;
+		}else{
+			numOfCell++;	//if index already hand cell, than add one more cell.
+			continue;
 		}
-		cout << endl;
-	}
+//		cout << "[" << col << "]" << "[" << rol << "]"<< endl;
+	}//for
+}//generation cell
 
 
-	return 0;
-}
-
-void determineValidPosition(int rol,int cow){
-	if((rol-1) < 0){
-		return;
-	}else if((cow-1) < 0){
-		return;
-	}else if((rol+1) > 8){
-		return;
-	}else if((cow+1) > 8){
-		return;
-	}
 
 
-}
+
+
+

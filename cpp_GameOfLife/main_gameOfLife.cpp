8894df45fdd72fa4bcc757435b2efb2486faeadf
bingr <48603790+err03@users.noreply.github.com>
2021-03-21 04:34:27 +0800

    file change

--- cpp_GameOfLife/main_gameOfLife.cpp
+++ cpp_GameOfLife/main_gameOfLife.cpp
@@ -12,12 +12,25 @@
 #include <stdlib.h>
 
 //#include "Graph.h"
-#include "DetermineFunctions.h"
+//#include "DetermineFunctions.h"
 //#include "Cell.h"
 using namespace std;
 
 void generateCell();
 void drawGraph();
+
+bool determineDeath(int,int);
+bool determineBirth(int,int);
+void determineNearCell(int&,int,int);
+
+void determineTop(int&,int,int);
+void determineLeft(int&,int,int);
+void determineRight(int&,int,int);
+void determineDown(int&,int,int);
+void determineTop_Left(int&,int,int);
+void detemineTop_Right(int&,int,int);
+void determineDown_Left(int&,int,int);
+void determineDown_Right(int&,int,int);
 
 const int ROL = 8,COL = 8;
 char lives[ROL][COL];
@@ -25,6 +38,35 @@ char cell = '*';	//change cell's appearance
 
 int main() {
 	generateCell();
+	drawGraph();
+	int i = 1;
+	int c = 1;
+	for(int i=0;i<ROL;i++){
+		for(int c=0;c<COL;c++){
+			if(lives[i][c] == cell){
+//				cout << "yes cell"<< endl;
+				if(determineDeath(i,c)){
+					lives[i][c] = ' ';
+//					cout << "cell death"<< endl;
+				}else{
+					lives[i][c] = cell;
+//					cout << "cell birth"<< endl;
+				}
+			}else{
+//				cout << "no cell"<< endl;
+				if(determineBirth(i,c)){
+					lives[i][c] = cell;
+//					cout << "yes birth"<< endl;
+				}else{
+//					cout << "no birth"<< endl;
+				}
+			}//if else
+		}//for
+	}//for
+	cout << "-----------------------------------------" << endl;
+	cout << "-----------------------------------------" << endl;
+	cout << "-----------------------------------------" << endl;
+
 	drawGraph();
 	return 0;
 }//main
@@ -67,9 +109,121 @@ void generateCell(){
 	}//for
 }//generation cell
 
+//------------------------------ determine function -------------------------------------------------------
+bool determineDeath(int rol,int col){
+	int death = 0;
+	determineNearCell(death,rol,col);
 
+	if(death > 3 || death < 2){
+		return true;
+	}
+	return false;
+}//determine death
 
+bool determineBirth(int rol,int col){
+	int birth = 0;
+	determineNearCell(birth,rol,col);
 
+	if(birth == 3){
+		return true;
+	}
+	return false;
+}//determine birth
 
+void determineNearCell(int& count,int rol,int col){
+	determineTop(count,rol,col);
+	determineLeft(count,rol,col);
+	determineRight(count,rol,col);
+	determineDown(count,rol,col);
+	determineTop_Left(count,rol,col);
+	detemineTop_Right(count,rol,col);
+	determineDown_Left(count,rol,col);
+	determineDown_Right(count,rol,col);
+}//determine near cell
 
+void determineTop(int& count,int rol,int col){
+	int top = rol - 1;
+	if(top < 0){
+		return;
+	}
+	if(lives[top][col] == cell){
+//		cout << "top +" << endl;
+		count++;
+	}//if
+} //determineTop
+void determineLeft(int& count,int rol,int col){
+	int left = col - 1;
+	if(left < 0){
+		return;
+	}
+	if(lives[rol][left] == cell){
+//		cout << "left +" << endl;
+		count++;
+	}//if
+} //determineLeft
+void determineRight(int& count,int rol,int col){
+	int right = col + 1;
+	if(right > COL){
+		return;
+	}
+	if(lives[rol][right] == cell){
+//		cout << "right +" << endl;
+		count++;
+	}//if
+} //determineRight
+void determineDown(int& count,int rol,int col){
+	int down = rol + 1;
+	if(down > ROL){
+		return;
+	}
+	if(lives[down][col] == cell){
+//		cout << "down +" << endl;
+		count++;
+	}//if
+} //detemineDown
 
+//--------------------------
+void determineTop_Left(int& count,int rol,int col){
+	int top = rol - 1;
+	int left = col - 1;
+	if(left < 0){
+		return;
+	}
+	if(lives[top][left] == cell){
+//		cout << "top left +" << endl;
+		count++;
+	}//if
+} //detemineTop_Left
+void detemineTop_Right(int& count,int rol,int col){
+	int top = rol - 1;
+	int right = col + 1;
+	if(top < 0){
+		return;
+	}
+	if(lives[top][right] == cell){
+//		cout << "top right +" << endl;
+		count++;
+	}//if
+} //detemineTop_Right
+void determineDown_Left(int& count,int rol,int col){
+	int down = rol + 1;
+	int left = col - 1;
+	if(left < 0){
+		return;
+	}
+	if(lives[down][left] == cell){
+//		cout << "down left +" << endl;
+		count++;
+	}//if
+} //determineDown_Left
+void determineDown_Right(int& count,int rol,int col){
+	int down = rol + 1;
+	int right = col + 1;
+	if(down > ROL){
+		return;
+	}
+	if(lives[down][right] == cell){
+//		cout << "down right +" << endl;
+		count++;
+	}//if
+} //determineDown_Right

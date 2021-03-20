f77930fc3d06e29e2a96292ad23c32cc360cdf35
bingr <48603790+err03@users.noreply.github.com>
2021-03-21 06:16:33 +0800

    final Update

--- cpp_GameOfLife/main_gameOfLife.cpp
+++ cpp_GameOfLife/main_gameOfLife.cpp
@@ -22,6 +22,7 @@ void drawGraph();
 bool determineDeath(int,int);
 bool determineBirth(int,int);
 void determineNearCell(int&,int,int);
+bool determineNoMoreCell();
 
 void determineTop(int&,int,int);
 void determineLeft(int&,int,int);
@@ -32,47 +33,48 @@ void detemineTop_Right(int&,int,int);
 void determineDown_Left(int&,int,int);
 void determineDown_Right(int&,int,int);
 
-const int ROL = 8,COL = 8;
-char lives[ROL][COL];
+const int ROW = 8,COL = 8;
+char lives[ROW][COL];
 char cell = '*';	//change cell's appearance
-
+int numOfCell = 10;//how many cell you want?
+int generationTime = 20;	//how many generation time you want?
 int main() {
+	int generation = 0;
 	generateCell();
+	cout << "Generation:" << generation<< endl;
 	drawGraph();
-	int i = 1;
-	int c = 1;
-	for(int i=0;i<ROL;i++){
-		for(int c=0;c<COL;c++){
-			if(lives[i][c] == cell){
-//				cout << "yes cell"<< endl;
-				if(determineDeath(i,c)){
-					lives[i][c] = ' ';
-//					cout << "cell death"<< endl;
-				}else{
-					lives[i][c] = cell;
-//					cout << "cell birth"<< endl;
-				}
-			}else{
-//				cout << "no cell"<< endl;
-				if(determineBirth(i,c)){
-					lives[i][c] = cell;
-//					cout << "yes birth"<< endl;
-				}else{
-//					cout << "no birth"<< endl;
-				}
-			}//if else
-		}//for
-	}//for
-	cout << "-----------------------------------------" << endl;
-	cout << "-----------------------------------------" << endl;
-	cout << "-----------------------------------------" << endl;
+//	int i=1,c=2;
+	for(int g=0;g<generationTime;g++){
 
-	drawGraph();
+		for(int i=0;i<ROW;i++){
+			for(int c=0;c<COL;c++){
+				if(lives[i][c] == cell){	//if this index have cell
+					if(determineDeath(i,c)){	//determine the death
+						lives[i][c] = ' ';
+					}else{
+						lives[i][c] = cell;		//if near cell have 2, keep it
+					}//if else
+				}else{						//else this index have no cell
+					if(determineBirth(i,c)){	//determine the birth
+						lives[i][c] = cell;
+					}//if
+				}//if else -current index have cell or not
+			}//for_loop the Column
+		}//for_loop the Row
+
+		generation++;
+
+		cout << "Generation:" << generation<< endl;
+		drawGraph();
+		if(determineNoMoreCell()){
+			exit(0);
+		}//if the no more cell
+	}//for_loop the generation
 	return 0;
 }//main
 //------------------------------ Graph Functions -------------------------------------------------------
 void drawGraph(){
-	for(int i=0;i<ROL;i++){		//print out the graph
+	for(int i=0;i<ROW;i++){		//print out the graph
 		for(int c=0;c<COL;c++){
 			if(c == (COL-1)){
 				cout << " " << lives[i][c] << " "<<endl;
@@ -80,7 +82,7 @@ void drawGraph(){
 			}//if
 			cout << " "<<lives[i][c]<<" |";
 		}//for
-		if(i != (ROL-1)){
+		if(i != (ROW-1)){
 			cout <<setw(4*COL)<<setfill('-')<< "" <<endl;	//set the separate line by COL length
 		}//if
 	}//for
@@ -88,16 +90,15 @@ void drawGraph(){
 
 //------------------------------ Cell Functions -------------------------------------------------------
 void generateCell(){
-	for(int i=0;i<ROL;i++){
+	for(int i=0;i<ROW;i++){
 		for(int c=0;c<COL;c++){
 			lives[i][c] = ' ';
 		}//for
 	}//for
 
-	int numOfCell = 25;		//how many cell you want?
 	srand(time(NULL));
 	for(int i=0;i<numOfCell;i++){
-		int rol = rand() % ROL;
+		int rol = rand() % ROW;
 		int col = rand() % COL;
 		if(lives[rol][col] != cell){
 			lives[rol][col] = cell;
@@ -105,7 +106,7 @@ void generateCell(){
 			numOfCell++;	//if index already hand cell, than add one more cell.
 			continue;
 		}
-//		cout << "[" << col << "]" << "[" << rol << "]"<< endl;
+//		cout << "[" << col << "]" << "[" << ROW << "]"<< endl;
 	}//for
 }//generation cell
 
@@ -114,7 +115,7 @@ bool determineDeath(int rol,int col){
 	int death = 0;
 	determineNearCell(death,rol,col);
 
-	if(death > 3 || death < 2){
+	if(death > 3 || death < 2){		//rule: if near cells is more than 3, or less than 2
 		return true;
 	}
 	return false;
@@ -124,12 +125,15 @@ bool determineBirth(int rol,int col){
 	int birth = 0;
 	determineNearCell(birth,rol,col);
 
-	if(birth == 3){
+	if(birth == 3){		//rule: if near cells is exactly 3
 		return true;
 	}
 	return false;
 }//determine birth
 
+/*
+ * determine the position about 8 positions
+ */
 void determineNearCell(int& count,int rol,int col){
 	determineTop(count,rol,col);
 	determineLeft(count,rol,col);
@@ -141,6 +145,21 @@ void determineNearCell(int& count,int rol,int col){
 	determineDown_Right(count,rol,col);
 }//determine near cell
 
+bool determineNoMoreCell(){
+	int cellCount = 0;
+	for(int i=0;i<ROW;i++){
+		for(int c=0;c<COL;c++){
+			if(lives[i][c] == cell){
+				cellCount++;
+			}//if
+		}//for
+	}//for
+	if(cellCount == 0){
+		return true;
+	}//if
+	return false;
+}//determine no more cell
+//--------------------------Determine cell's index position
 void determineTop(int& count,int rol,int col){
 	int top = rol - 1;
 	if(top < 0){
@@ -173,7 +192,7 @@ void determineRight(int& count,int rol,int col){
 } //determineRight
 void determineDown(int& count,int rol,int col){
 	int down = rol + 1;
-	if(down > ROL){
+	if(down > ROW){
 		return;
 	}
 	if(lives[down][col] == cell){
@@ -219,7 +238,7 @@ void determineDown_Left(int& count,int rol,int col){
 void determineDown_Right(int& count,int rol,int col){
 	int down = rol + 1;
 	int right = col + 1;
-	if(down > ROL){
+	if(down > ROW){
 		return;
 	}
 	if(lives[down][right] == cell){
